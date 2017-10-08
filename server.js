bcf2296cfa4107348160bde2c87e18b832324d9d
Logan Absher <absherlogan@gmail.com>
2017-10-08 14:12:31 -0700

    fixed server redirect

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/*', (req, res) => res.redirect('/index.html'));
+app.get('/*', (req, res) => res.sendFile('./index.html'));
 
 app.listen(PORT, function(){
   console.log('server up:', PORT);
