99099d9aa1e2833fb466ae6295b00dabfbfadd23
Logan Absher <absherlogan@gmail.com>
2017-12-02 13:23:27 -0800

    tabbing between pages working correctly

--- public/routes/routes.js
+++ public/routes/routes.js
@@ -1,7 +1,32 @@
 'use strict';
 
-page('/', homeController.init);
-page('/contact', contactController.init);
-page('/about', aboutController.init);
+const page = require('../vendors/scripts/page.js');
+
+const controller = {};
+
+controller.home = function(){
+  $('#about').hide();
+  $('#contact').hide();
+  $('title').html('welcome')
+  $('#home').fadeIn('slow');
+};
+
+controller.contact = function(){
+  $('#home').hide();
+  $('#about').hide();
+  $('title').html('contact')
+  $('#contact').fadeIn('slow');
+};
+
+controller.about = function(){
+  $('#home').hide();
+  $('#contact').hide();
+  $('title').html('about us')
+  $('#about').fadeIn('slow');
+};
+
+page('/', controller.home);
+page('/contact', controller.contact);
+page('/about', controller.about);
 
 page();
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // TODO: rework routes
-app.get('/*', (request, response) => response.sendFile(`${__dirname}/public/index.html`, {root: '.'}));
+app.get('/*', (request, response) => response.sendFile(`./public/index.html`, {root: '.'}));
 
 app.listen(PORT, function(){
   console.log('server up:', PORT);
