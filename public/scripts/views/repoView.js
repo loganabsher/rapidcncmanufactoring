30c60088dba346680987215a088c55e424057439
Logan Absher <absherlogan@gmail.com>
2018-02-22 12:19:09 -0800

    bootstrap setup

--- public/scripts/views/repoView.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-(function(module){
-  // creating a constant variable that holds all functions
-  const repoView = {}
-  // passing all functions out of the enclosing function through the module parameter
-  module.repoView = repoView;
-})(window);
